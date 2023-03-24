c4b3263ba780db238558ecb48a7ddb9650e03737
Lidia D <lidia.khmylko@gmail.com>
2023-03-23 22:18:49 -0700

    Pass initial array

---
+++ 02_same_array/same_array.js
@@ -0,0 +1,25 @@
+const comp = function(array1, array2) {
+  if (!Array.isArray(array1) || !array1.length) {
+    return false;
+  }
+   if (!Array.isArray(array2) || !array2.length) {
+    return false;
+  }
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  if (array1.length === 0 && array2.length === 0) {
+    return true;
+  }
+  
+  const temp1 = Array.from(array1).sort();
+  const temp2 = Array.from(array2).sort();
+  for (let i = 0; i < temp1.length; i++) {
+    if (temp1[i] **2 !== temp2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+module.exports = comp;
---
+++ 02_same_array/same_array.spec.js
@@ -0,0 +1,15 @@
+const comp = require('./same_array');
+
+describe('Same Array', function() {
+  test('Passes initial array', function() {
+    let a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+    let a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
+    expect(comp(a1, a2)).toEqual(true);
+  });
+
+  test('Does not pass changed initial array', function() {
+    let a1 = [155, 144, 19, 161, 19, 144, 19, 11];
+    let a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
+    expect(comp(a1, a2)).toEqual(false);
+  });
+});
